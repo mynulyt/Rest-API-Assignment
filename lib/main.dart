2d73110bac8f740cf212d2cfc9899baede655ada
Mynul Alam <mynulalam27@gmail.com>
2025-08-27 23:02:35 +0600

    add container

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Material App Bar')),
-        body: const Center(child: Text('Hello World')),
-      ),
-    );
+    return MaterialApp(home: Scaffold(body: Container()));
   }
 }
