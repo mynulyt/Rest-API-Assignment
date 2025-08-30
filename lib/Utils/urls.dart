e7786280e6f5be487af5d17b5ac291d55228e978
Mynul Alam <mynulalam27@gmail.com>
2025-08-30 23:58:17 +0600

    utils

---
+++ lib/Utils/urls.dart
@@ -0,0 +1,5 @@
+class Urls {
+  static const _baseUrl = 'http://35.73.30.144:2008/api/v1';
+  static const getProductsUrl = '$_baseUrl/ReadProduct';
+  static String deleteProductsUrl(String id) => '$_baseUrl/DeleteProduct/$id';
+}
